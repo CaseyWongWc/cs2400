97b09188d993dac886ee2106479800dadbcc130a
Casey Wong <caseywongwc@gmail.com>
2024-05-08 18:41:56 -0700

    fixed the DFS

--- project9graphs/matrixgraph.java
+++ project9graphs/matrixgraph.java
@@ -1,8 +1,11 @@
 package project9graphs;
 import java.util.HashSet;
-//import java.util.Iterator;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.Vector;
 
+import project2stack.LinkedStack;
+import project2stack.StackInterface;
 import project3queue.LinkedQueue;
 import project3queue.QueueInterface;
 
@@ -30,6 +33,7 @@ public class matrixgraph<E> implements GraphInterface <E>
     {
         return edges[source][target] = true;
     }
+
 
     @Override
     public E getLabel(int vertex) 
@@ -112,7 +116,7 @@ public class matrixgraph<E> implements GraphInterface <E>
    
 
     public QueueInterface<E> getBreadthFirstTraversal(E origin) 
-    {
+    {System.out.println("BFS:");
         Set<E> visited = new HashSet<>(); // Tracks visited vertices
         
         QueueInterface<E> traversalOrder = new LinkedQueue<>();
@@ -147,6 +151,42 @@ public class matrixgraph<E> implements GraphInterface <E>
         return traversalOrder;
     }
     
+    @Override
+    public QueueInterface<E> getDepthFirstTraversal(E origin) 
+    {
+    System.out.println("DFS:");
+    Set<E> visited = new HashSet<>();
+
+    QueueInterface<E> traversalOrder = new LinkedQueue<>();
+    StackInterface<E> vertexStack = new LinkedStack<>();
+
+    vertexStack.push(origin);
+    while (!vertexStack.isEmpty()) 
+    {
+        E topVertex = vertexStack.pop();
+
+        if (!visited.contains(topVertex)) 
+        {
+            visited.add(topVertex); // Mark the vertex as visited here
+            traversalOrder.enqueue(topVertex); // Store the visited vertex
+            
+            // Fetch the neighbors of the current vertex
+            int[] neighbors = neighbors(getindex(topVertex)); 
+            for (int i = neighbors.length - 1; i >= 0; i--) 
+            { 
+                // Iterate in reverse to maintain correct DFS order
+                E neighborLabel = getLabel(neighbors[i]);
+                if (!visited.contains(neighborLabel)) 
+                {
+                    vertexStack.push(neighborLabel); // Push unvisited neighbors
+                }
+            }
+        }
+    }
+    System.out.print("DepthFirstTraversal: ");
+    traversalOrder.printQueue();
+    return traversalOrder;
+}
 
 
     public void printgraph() 
@@ -169,11 +209,7 @@ public class matrixgraph<E> implements GraphInterface <E>
         }
     }
 
-    @Override
-    public QueueInterface<E> getDepthFirstTraversal(E origin) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDepthFirstTraversal'");
-    }
+
     
 }
 
@@ -182,29 +218,56 @@ class adjGraph
     public static void main(String[] args) 
     {
         //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
-        GraphInterface<String> hi = new matrixgraph<>(5);
+        // GraphInterface<String> hi = new matrixgraph<>(5);
 
-        hi.setLabel(0,"0");
-        hi.setLabel(1,"1");
-        hi.setLabel(2,"2");
-        hi.setLabel(3,"3");
-        hi.setLabel(4,"4");
+        // hi.setLabel(0,"0");
+        // hi.setLabel(1,"1");
+        // hi.setLabel(2,"2");
+        // hi.setLabel(3,"3");
+        // hi.setLabel(4,"4");
 
-        hi.addEdge(0, 1);
-        hi.addEdge(0, 2);
-        hi.addEdge(1, 0);
-        hi.addEdge(1, 2);
-        hi.addEdge(1, 3);
-        hi.addEdge(2, 0);
-        hi.addEdge(2, 1);
-        hi.addEdge(2, 4);
-        hi.addEdge(3, 1);
-        hi.addEdge(3, 4);
-        hi.addEdge(4, 2);
-        hi.addEdge(4, 3);
+        // hi.addEdge(0, 1);
+        // hi.addEdge(0, 2);
+        // hi.addEdge(1, 0);
+        // hi.addEdge(1, 2);
+        // hi.addEdge(1, 3);
+        // hi.addEdge(2, 0);
+        // hi.addEdge(2, 1);
+        // hi.addEdge(2, 4);
+        // hi.addEdge(3, 1);
+        // hi.addEdge(3, 4);
+        // hi.addEdge(4, 2);
+        // hi.addEdge(4, 3);
 
-        hi.neighbors(3);
-        hi.printgraph();
-        hi.getBreadthFirstTraversal("0");
+        // hi.neighbors(3);
+        // hi.printgraph();
+        // hi.getBreadthFirstTraversal("0");
+
+        GraphInterface<String> sample = new matrixgraph<>(9);
+
+        sample.setLabel(0, "A");
+        sample.setLabel(1, "B");
+        sample.setLabel(2, "C");
+        sample.setLabel(3, "D");
+        sample.setLabel(4, "E");
+        sample.setLabel(5, "F");
+        sample.setLabel(6, "G");
+        sample.setLabel(7, "H");
+        sample.setLabel(8, "I");
+
+        sample.addEdge(0, 1);sample.addEdge(0, 3);sample.addEdge(0, 4);
+        sample.addEdge(1, 4);
+        sample.addEdge(2, 1);
+        sample.addEdge(3, 6);
+        sample.addEdge(4, 5);sample.addEdge(4, 7);
+        sample.addEdge(5, 2);sample.addEdge(5, 7);
+        sample.addEdge(6, 7);
+        sample.addEdge(7, 8);
+        sample.addEdge(8, 5);
+        sample.printgraph();
+        sample.getBreadthFirstTraversal("A");
+        sample.getDepthFirstTraversal("A");
+
+    
     }
 }
